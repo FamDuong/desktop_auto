a351de6aec5650107b1f13bd131523d05f476d45
nguyenquangtay <quangtay88@gmail.com>
2022-11-04 15:14:16 +0700

    Add get username

--- src/tests/test_start_exe_file.py
+++ src/tests/test_start_exe_file.py
@@ -1,5 +1,6 @@
-
 from pywinauto import Application
+
+from src.utils import os_utils
 
 
 def open_coccoc_install_dialog(file_name='CocCocSetup.exe'):
@@ -26,4 +27,19 @@ def open_install_dialog(file_name='CocCocSetup.exe'):
 
 def test_open_install_dialog():
     open_install_dialog()
-    
+
+
+"""Dynamic get username"""
+
+
+def open_install_dialog2(file_name='CocCocSetup.exe'):
+    # Start file setup
+    try:
+        Application(backend='uia').start(rf'C:\\Users\\{os_utils.get_username()}\\Downloads\\{file_name}',
+                                         retry_interval=2, timeout=30)
+    except Exception as e:
+        print(e)
+
+
+def test_open_install_dialog2():
+    open_install_dialog2()
