9f28cd15b5324d6ff884c7ba12b9c726c7f67355
nguyenquangtay <quangtay88@gmail.com>
2023-02-12 10:32:22 +0700

    add multiple browsers configuration

---
+++ src/tests/fixtures_test/test_coccoc_search.py
@@ -0,0 +1,36 @@
+import time
+
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+
+
+def test_coccoc_search(web_driver):
+    # Access the coccoc search home page
+    web_driver.get('https://coccoc.com/search')
+
+    # Enter the searching text and click to search
+
+    search_box_locator = web_driver.find_element(By.CSS_SELECTOR, 'input[class="suggestBox-K4SFm"]')
+    search_box_locator.send_keys('tinhte' + Keys.RETURN)
+
+    # Verify the text 'tinhte' appears
+    search_result_locator = web_driver.find_element(By.CSS_SELECTOR, 'div[class="newRightWiki-YyhNt"] div:nth-child(1)')
+    assert 'Tinh Tế' == search_result_locator.text
+
+    time.sleep(5)
+
+
+def test_coccoc_search_2(web_driver):
+    # Access the coccoc search home page
+    web_driver.get('https://coccoc.com/search')
+
+    # Enter the searching text and click to search
+
+    search_box_locator = web_driver.find_element(By.CSS_SELECTOR, 'input[class="suggestBox-K4SFm"]')
+    search_box_locator.send_keys('coccoc' + Keys.RETURN)
+
+    # Verify the text 'Cốc Cốc' appears
+    search_result_locator = web_driver.find_element(By.CSS_SELECTOR, 'div[class="newRightWiki-YyhNt"] div:nth-child(1)')
+    assert 'Cốc Cốc' == search_result_locator.text
+
+    time.sleep(5)
